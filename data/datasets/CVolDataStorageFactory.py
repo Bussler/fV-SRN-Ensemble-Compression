25c14666d3c5f12fcd51582dd6909b6ef64eae77
bussler <maarten.bussler@gmail.com>
2022-09-30 11:26:42 +0200

    Add parser of multivariate and univariate data stroage to CVolDataStorage

--- data/datasets/CVolDataStorageFactory.py
+++ data/datasets/CVolDataStorageFactory.py
@@ -1,14 +1,57 @@
 import argparse
 
-from data.datasets.multivariate.data_storage import MultivariateEnsembleDataStorage
+from data.datasets.multivariate.data_storage import MultivariateEnsembleDataStorage, VARIABLE_NAMES
 from data.datasets.univariate.data_storage import VolumeDataStorage
 
 class CVolDataStorageFactory():
 
     @staticmethod
     def init_parser(parser: argparse.ArgumentParser):
-        # TODO implement summarizing parser for Uni/ Multi
-        raise NotImplementedError()
+        group = parser.add_argument_group('VolumeDataStorage')
+        example_pattern = ('"ScalarFlow/sim_{'
+                           + VolumeDataStorage.FILE_PATTERN_ENSEMBLE_KEY
+                           + ':06d}/density_{'
+                           + VolumeDataStorage.FILE_PATTERN_TIME_KEY
+                           + ':06d}.cvol"')
+        prefix = '--data-storage:'
+        group.add_argument(prefix + 'filename-pattern', type=str, help=f"""
+                file name pattern used to generate volume file names. 
+                To specify time and ensemble indices, the following keys are used:
+
+                time: {VolumeDataStorage.FILE_PATTERN_TIME_KEY}
+                ensemble: {VolumeDataStorage.FILE_PATTERN_ENSEMBLE_KEY}
+
+                Example: {example_pattern}
+                """) #M: dont't set required = True, since this is now distinguishing property to Multivariate
+        group.add_argument(prefix + 'base-path', type=str, default=None, help="""
+                base path to be prepended in front of file name pattern
+                """)
+        group.add_argument(prefix + 'ensemble:index-range', type=str, default=None, help="""
+                    Ranges used for the ensemble index. The indices are obtained via
+                    <code>range(*map(int, {ensemble_index_range}.split(':')))</code>
+
+                    Example: "0:10:2" (Default: "0:1")
+                """)
+        group.add_argument(prefix + 'timestep:index-range', type=str, default=None, help="""
+                    Ranges used for the keyframes for time interpolation. 
+                    At those timesteps, representative vectors are generated, optionally trained,
+                    and interpolated between timesteps
+                    The indices are obtained via <code>range(*map(int, {timestep-index-range}.split(':')))</code>
+
+                    Example: "0:10:2" (Default: "0:1")
+                """)
+        group.add_argument(prefix + 'variables', type=str, default=None, help=f"""
+                    Variable keys separated by ':'
+                    Choices: {VARIABLE_NAMES}
+                """)
+        group.add_argument(prefix + 'normalization', type=str, default=None, help=f"""
+                    normalization type to use
+                    Choices: {VARIABLE_NAMES}
+                """)
+        group.add_argument(prefix + 'disable-file-verification', action='store_false', dest='verify_files_exist', help="""
+                disable file verification for performance reasons
+                """)
+        group.set_defaults(verify_files_exist=True)
 
     # Factory pattern
     @classmethod
